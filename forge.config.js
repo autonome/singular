5204ee97cd0c1dcca0a31c4c2326c1cd1081db42
Steven Vandevelde <steven@tokono.ma>
2025-09-12 14:54:13 +0200

    feat: make appimage through electron-forge

---
+++ forge.config.js
@@ -0,0 +1,14 @@
+export default {
+  makers: [
+    {
+      name: "@reforged/maker-appimage",
+      config: {
+        options: {
+          bin: "Singular",
+          // categories: ["Network"],
+          // icon: "path/to/icon.svg",
+        },
+      },
+    },
+  ],
+};
